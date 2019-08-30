6eea28a44befdd399f205a4e41921b823c829bad
Vasu Goel <goelvasu0712@gmail.com>
2019-08-31 01:00:02 +0530

    R Script to create time series plot for fertility

--- time-series.R
+++ time-series.R
@@ -21,3 +21,13 @@ gapminder %>% filter(country %in% countries & !is.na(fertility)) %>%
   ggplot(aes(year,fertility, col = country)) +
   geom_line()
 ggsave('figs/time_series-3.png')
+
+# labelled fertility vs time plot for 'South Korea' and 'Germany'
+labels <- data.frame(country = countries, x = c(1975,1965), y = c(60,72))
+gapminder %>% 
+  filter(country %in% countries) %>% 
+  ggplot(aes(year, life_expectancy, col = country)) +
+  geom_line() +
+  geom_text(data = labels, aes(x, y, label = country), size = 5) +
+  theme(legend.position = "none")
+ggsave('figs/time_series-4.png')
