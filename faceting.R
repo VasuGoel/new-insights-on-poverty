c5e8d41844f5cbbff5255ab111326569f38af4aa
Vasu Goel <goelvasu0712@gmail.com>
2019-08-31 00:42:55 +0530

    R Script to visualize life_expectancy vs fertility with faceted plots

--- faceting.R
+++ faceting.R
@@ -15,3 +15,13 @@ filter(gapminder, year%in%c(1962, 2012)) %>%
   geom_point() +
   facet_grid(. ~ year)
 ggsave('figs/faceting-2.png')
+
+# facet_wrap() to display each plots with own viewable dimensions
+years <- c(1962, 1980, 1990, 2000, 2012)
+continents <- c("Europe", "Asia")
+gapminder %>% 
+  filter(year %in% years & continent %in% continents) %>%
+  ggplot( aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_wrap(~year) 
+ggsave('figs/faceting-3.png')
